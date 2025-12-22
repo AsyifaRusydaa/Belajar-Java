ab538ca540e340b1d9e6f0165c8a8600c402a9a4
Asyifa Rusyda Alifiana <syifasyifaaa804@gmail.com>
2025-12-22 22:08:04 +0700

    Add files via upload

---
+++ Operator/src/com/aku/operator/OperatorAssignment.java
@@ -0,0 +1,10 @@
+package com.aku.operator;
+
+public class OperatorAssignment {
+    public static void main(String[] args) {
+        int value = 2;
+        int anotheValue = 19;
+        System.out.println("Data pada value adalah ->>"+value);
+        System.out.println("Data pada anotherValue adalah ->>"+anotheValue);
+    }
+}
