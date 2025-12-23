d027f5666f09314d1d16d775d2cc0cf52c02ec4d
Asyifa Rusyda Alifiana <syifasyifaaa804@gmail.com>
2025-12-23 18:33:45 +0700

    Update OperatorAssignment.java

--- Operator/src/com/aku/operator/OperatorAssignment.java
+++ Operator/src/com/aku/operator/OperatorAssignment.java
@@ -12,10 +12,11 @@ public class OperatorAssignment {
         System.out.println();
         System.out.println("Saat saya melihat nilai hasil ulangan saya bernilai "+a+". Saya merasa sedih dan mencoba mengeceknya kembali.");
         a = 100;
-        System.out.println("Setelah saya cek kembali ternyata semua jawaban saya benar dan saya mendapatkan nilai "+a);
+        System.out.println("Setelah saya cek kembali ternyata semua jawaban saya benar dan saya mendapatkan nilai "+a+".");
         System.out.println();
         System.out.println("Dan dari situ saya sadar kalau saya memang kece");
     }
 }
+
 
 
