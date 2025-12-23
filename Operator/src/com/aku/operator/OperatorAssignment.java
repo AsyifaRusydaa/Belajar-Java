1ac687fc126ca1086fbc13fc0368989b9d386e6f
Asyifa Rusyda Alifiana <syifasyifaaa804@gmail.com>
2025-12-23 18:16:32 +0700

    Update OperatorAssignment.java

--- Operator/src/com/aku/operator/OperatorAssignment.java
+++ Operator/src/com/aku/operator/OperatorAssignment.java
@@ -2,9 +2,12 @@ package com.aku.operator;
 
 public class OperatorAssignment {
     public static void main(String[] args) {
-        int value = 2;
-        int anotheValue = 19;
-        System.out.println("Data pada value adalah ->>"+value);
-        System.out.println("Data pada anotherValue adalah ->>"+anotheValue);
+        int x = 24;
+        int y = 8;
+        int z = 2009;
+        System.out.println("Saya lahir pada tanggal "+x);
+        System.out.println("Pada bulan "+y);
+        System.out.println("Di tahun "+z);
     }
 }
+
