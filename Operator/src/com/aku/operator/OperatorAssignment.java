2e00c35a414548d6ca53c48a0478f93054d35454
Asyifa Rusyda Alifiana <syifasyifaaa804@gmail.com>
2025-12-23 18:31:53 +0700

    Update OperatorAssignment.java

--- Operator/src/com/aku/operator/OperatorAssignment.java
+++ Operator/src/com/aku/operator/OperatorAssignment.java
@@ -8,6 +8,14 @@ public class OperatorAssignment {
         System.out.println("Saya lahir pada tanggal "+x);
         System.out.println("Pada bulan "+y);
         System.out.println("Di tahun "+z);
+        int a = 62;
+        System.out.println();
+        System.out.println("Saat saya melihat nilai hasil ulangan saya bernilai "+a+". Saya merasa sedih dan mencoba mengeceknya kembali.");
+        a = 100;
+        System.out.println("Setelah saya cek kembali ternyata semua jawaban saya benar dan saya mendapatkan nilai "+a);
+        System.out.println();
+        System.out.println("Dan dari situ saya sadar kalau saya memang kece");
     }
 }
+
 
